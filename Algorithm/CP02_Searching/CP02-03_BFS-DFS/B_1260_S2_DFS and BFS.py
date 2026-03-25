3f0ec40028900ae8b9eca0519faf260d14d0bc75
Groovy52 <kkm5020@gachon.ac.kr>
2026-03-25 20:38:25 +0900

    dfs and bfs 0325 commit_resolve

--- Algorithm/CP02_Searching/CP02-03_BFS-DFS/B_1260_S2_DFS and BFS.py
+++ Algorithm/CP02_Searching/CP02-03_BFS-DFS/B_1260_S2_DFS and BFS.py
@@ -1,5 +1,95 @@
 # 1260: DFS와 BFS
 
+# 틀린 코드: sort 정렬 문제
+N, M, V = map(int, input().split())
+li = [list(map(int, input().split())) for _ in range(M)]
+li.sort()
+
+E = [[] for _ in range(N+1)]
+for s, e in li:
+    E[s].append(e)
+    E[e].append(s)
+
+v = [False]*(N+1)
+res = []
+
+def dfs(E, V):
+    v[V] = True
+    res.append(V)
+
+    for nxt in E[V]:
+        if not v[nxt]:
+            dfs(E, nxt)
+            
+def bfs(E, V):
+    from collections import deque 
+
+    v = [False]*(N+1)
+    q = deque()
+    res = []
+    q.append(V)
+    v[V] = True
+
+    while q:
+        curr = q.popleft()
+        res.append(curr)
+        for nxt in E[curr]:
+            if not v[nxt]:
+                q.append(nxt)
+                v[nxt] = True
+    return res
+
+dfs(E, V)
+print(*res)
+print(*bfs(E, V))
+
+
+# 에러 해결
+N, M, V = map(int, input().split())
+E = [[] for _ in range(N+1)]
+for _ in range(M):
+    s, e = map(int, input().split())
+    E[s].append(e)
+    E[e].append(s)
+
+for e in E:
+    e.sort()
+
+v = [False]*(N+1)
+res = []
+
+def dfs(E, V):
+    v[V] = True
+    res.append(V)
+
+    for nxt in E[V]:
+        if not v[nxt]:
+            dfs(E, nxt)
+            
+def bfs(E, V):
+    from collections import deque 
+
+    v = [False]*(N+1)
+    q = deque()
+    res = []
+    q.append(V)
+    v[V] = True
+
+    while q:
+        curr = q.popleft()
+        res.append(curr)
+        for nxt in E[curr]:
+            if not v[nxt]:
+                q.append(nxt)
+                v[nxt] = True
+    return res
+
+dfs(E, V)
+print(*res)
+print(*bfs(E, V))
+
+
+# 깔끔하게 작성
 N, M, V = map(int, input().split())
 adj = [[] for _ in range(N+1)] # 정점 번호가 1부터 시작하기 때문에, N이 아니고 N+1
 for _ in range(M):
